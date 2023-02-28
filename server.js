85807ce9292635512e8a8b60ad2d1fc7f1749594
AyeMishDior <itsamish02@gmail.com>
2023-02-27 20:08:36 -0600

    final commit

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/social-network-api',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
-
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
